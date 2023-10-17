4160fca7e5a4f9c48eb3cba9640d2dddd773078b
thais <thais.miranda.g@gmail.com>
2023-10-16 22:08:34 -0300

    projeto to do list

---
+++ todolist/src/main/java/br/com/thais/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.thais.todolist.task;
+import java.util.UUID;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+    
+}
